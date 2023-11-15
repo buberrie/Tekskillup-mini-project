9bffdf7af04ae28192702e7f4c84d372189d33c0
Oluebube5 <oluebubechukwugloria@gmail.com>
2023-11-15 21:41:37 +0100

    first commit

--- script/index.js
+++ script/index.js
@@ -17,3 +17,55 @@ document.onclick = function (e) {
     navBar.classList.remove('active')
     }
 };
+
+// email input validation
+
+const emailEl = document.querySelector('#email');
+const button = document.querySelector('.subcribe')
+
+const checkEmail = () => {
+    let valid = false;
+    const email = emailEl.value.trim();
+    if (!isRequired(email)) {
+        showError(emailEl, 'Email cannot be blank.');
+    } else if (!isEmailValid(email)) {
+        showError(emailEl, 'Email is not valid.')
+    } else {
+        showSuccess(emailEl);
+        valid = true;
+    }
+    return valid;
+};
+
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+};
+
+const isRequired = value => value === '' ? false : true;
+
+const showError = (input) => {
+    // get the form-field element
+    const formField = input;
+    // add the error class
+    formField.classList.add('error');
+
+};
+
+const showSuccess = (input) => {
+    // get the form-field element
+    const formField = input;
+
+    // remove the error class
+    formField.classList.remove('error');
+}
+
+button.addEventListener('click', function (e) {
+    let isEmailValid = checkEmail()
+
+    let isFormValid = isEmailValid 
+
+    if (isFormValid) {
+        alert('successful');
+    }
+})
